d08854e9de6767134eee2e38af507fc462147a59
stevenzg <guoyinfeng@live.com>
2019-01-18 23:00:44 +0800

    Updates

---
+++ precache-manifest.baabb80023be7304b54804e642192110.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6be875f181a91d5feca1",
+    "url": "/travel/static/js/runtime~main.6be875f1.js"
+  },
+  {
+    "revision": "3d76bc772a97c2b75fe4",
+    "url": "/travel/static/js/main.3d76bc77.chunk.js"
+  },
+  {
+    "revision": "f7604ec8c4b0a66b5c46",
+    "url": "/travel/static/js/1.f7604ec8.chunk.js"
+  },
+  {
+    "revision": "3d76bc772a97c2b75fe4",
+    "url": "/travel/static/css/main.83bd4d7b.chunk.css"
+  },
+  {
+    "revision": "855641f9fd337d674b0d9dbb569921a2",
+    "url": "/travel/index.html"
+  }
+];
